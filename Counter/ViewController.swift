149b79d3e2a1fdcc4dc83be5e17fa8aedcbab495
Ad4mWest <adam101west@icloud.com>
2023-03-15 11:45:23 +0100

    When the app starts, the label shows the text "0"; When you click on the button, the counter value increases by 1 and is displayed in the label text.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var meaningOfCounter: UILabel!
+    var count = 0
+    
+    @IBAction func IncresingByOne(_ sender: Any) {
+        count += 1
+        meaningOfCounter.text = "Counter value: \(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 

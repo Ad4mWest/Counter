7574265039bdfbc59f64c219808f3c81f3c48e9e
Ad4mWest <adam101west@icloud.com>
2023-03-15 17:49:34 +0100

    At the start of the application, the label shows the text "0", and the text in the text field corresponds to the description of paragraph 3 of the task condition; When you click on the "+" or "-" button, the counter value changes to -1 (if it was not equal to 0) or +1. The counter value is reset to zero in the label text, and a line is added to the text field (according to paragraph 3 of the task condition); When you click on the reset button, the counter value is reset in the label text, and a line is added to the text field (according to paragraph 3 of the task condition). The buttons have colors and icons corresponding to the task conditions; The text field scrolls, but is blocked for editing by the user.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,13 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var meaningOfCounter: UILabel!
-    var count = 0
-    
-    @IBAction func IncresingByOne(_ sender: Any) {
-        count += 1
-        meaningOfCounter.text = "Counter value: \(count)"
+    @IBOutlet weak private var meaningOfCounter: UILabel!
+    @IBOutlet weak private var historyOfChanges: UITextView!
+
+    private var count = 0
+    private func newFormatDate() -> String {
+        let dateNow = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.dateFormat = "d MMM yy - HH:mm:ss"
+        return dateFormatter.string(from: dateNow)
     }
+    
+    @IBAction private func incresingByOne(_ sender: Any) {
+        count += 1
+        meaningOfCounter.textColor = .red
+        meaningOfCounter.text = "Counter value: \(count)"
+        historyOfChanges.text += "\n [\(newFormatDate())]: value changed to +1"
+    }
+    @IBAction private func decreaseByOne(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            meaningOfCounter.textColor = .blue
+            meaningOfCounter.text = "Counter value: \(count)"
+            historyOfChanges.text += "\n [\(newFormatDate())]: value changed to -1"
+        } else {
+            meaningOfCounter.textColor = .green
+            meaningOfCounter.text = "Counter value: \(count)"
+            historyOfChanges.text += "\n [\(newFormatDate())]: attempt to reduce the counter value below 0"
+        }
+    }
+    @IBAction private func resetCountValue(_ sender: Any) {
+        count = 0
+        meaningOfCounter.textColor = .orange
+        meaningOfCounter.text = "Counter value: \(count)"
+        historyOfChanges.text += "\n [\(newFormatDate())]: value reset"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
